07e536f0350cfd259becad98f16eb6160b0f098d
Unknwonnay <aidonayaka4@gmail.com>
2025-09-28 07:00:16 +0700

    starter file

--- src/server.js
+++ src/server.js
@@ -1,62 +1,25 @@
-// mengimpor dotenv dan menjalankan konfigurasinya
-require('dotenv').config();
+import Hapi from '@hapi/hapi';
+import notes from "./api/notes";
+import NotesService from "./services/inMemory/NotesService.js";
 
-const Hapi = require('@hapi/hapi');
-const ClientError = require('./exceptions/ClientError');
-
-const notes = require('./api/notes');
-const NotesService = require('./services/postgres/NotesService');
-const NotesValidator = require('./validator/notes');
-
-const users = require('./api/users');
-const UsersService = require('./services/postgres/UsersService');
-const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const notesService = new NotesService();
-  const usersService = new UsersService();
   const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
+    port : 5000,
+    host : 'localhost',
     routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
+      cors : {
+        origin : ['']
+      }
+    }
   });
 
-  await server.register([
-    {
-      plugin: notes,
-      options: {
-        service: notesService,
-        validator: NotesValidator,
-      },
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
     },
-    {
-      plugin: users,
-      options: {
-        service: usersService,
-        validator: UsersValidator,
-      },
-    },
-  ]);
-
-  server.ext('onPreResponse', (request, h) => {
-    // mendapatkan konteks response dari request
-    const { response } = request;
-
-    // penanganan client error secara internal.
-    if (response instanceof ClientError) {
-      const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
-      });
-      newResponse.code(response.statusCode);
-      return newResponse;
-    }
-
-    return h.continue;
   });
 
   await server.start();
